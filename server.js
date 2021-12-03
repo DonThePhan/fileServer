a4a3223f20e1a6a10ec97c7201eac94f47d724a0
Donny <donthephan@gmail.com>
2021-12-03 08:08:05 +0000

    updated messages between client & server

--- server.js
+++ server.js
@@ -9,24 +9,34 @@ server.listen(3000, () => {
 
 server.on('connection', (client) => {
   console.log('New client connected!');
-  client.write('Hello there!');
 
   client.setEncoding('utf8'); // interpret data as text
+  client.write(`You're connected! 
+  To RETRIEVE info, type "download <fileName.ext>"
+  i.e. "download dune.txt"
+  TRY IT!`);
+
   client.on('data', (data) => {
     data = data.replace('\n', '');
-    let dataArr = data.split(' ');
+    const dataArr = data.split(' ');
 
+    // CASE A) Client RETRIEVING data
     if (dataArr[0] === 'download') {
       let fileName = dataArr[1];
-      fs.readFile(`./${fileName}`, `utf8`, (err, data) => {
+
+      // look up requested file & try to send to client
+      fs.readFile(`./${fileName}`, `utf8`, (err, fileData) => {
         if (!err) {
-          client.write(data);
+          client.write(fileData);
+          console.log(`Client requested & RECEIVED data from ${fileName}`);
+        } else {
+          client.write(`File not Found`);
+          console.log(`Client requested & FAILED to receive data from ${fileName}`);
         }
       });
+      // CASE B) Client passing a REGULAR message
     } else {
       console.log('Message from client: ', data);
     }
   });
-
-  client.write('super specical secret message sent!');
 });
